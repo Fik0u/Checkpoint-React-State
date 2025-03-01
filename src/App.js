fbb4ce6ad472a160d004a574832cd1d852423df7
Fik0u <fikouch@gmail.com>
2025-03-01 10:20:37 +0100

    Checkpoint React State

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import './App.css';
-
 import React, { Component } from 'react'
+// The import of a our class Person from the Person.js file
 import Person from './components/Person';
 
+// Class based Component App
 export default class App extends Component {
-constructor(props) {
-    super(props)
-    this.state = {
-      show: false
-}
-}
-Toggle = () => {
+    constructor(props) {
+          super(props)
+          this.state = {
+              show: false // A boolean state to show or hide the Person component
+          }
+      }
+// A function to toggle the show state
+  Toggle = () => {
     this.setState({ show: !this.state.show })
-}
+  }
 
   render() {
     return (
       <div className="App">
-        <h1>Checkpoint React State</h1>
-        <button onClick={this.Toggle}>{this.state.show ? 'Hide' : 'Show'}</button>
+        <h1 style={{color: 'white'}}>React State</h1>
+        <button onClick={this.Toggle}>{this.state.show ? 'Hide' : 'Show'}</button> {/* Call of the function to show or hide */}  
         {this.state.show && <Person />}
       </div>
     )
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,37 +1,37 @@
 import React, { Component } from 'react'
-import pic from '../assets/pic.jpg'
+import pic from '../assets/pic.png'
 
+// Class based Component Person
 export default class Person extends Component {
     constructor(props) {
         super(props)
         this.person = {
-            fullName: 'Elon Musk',
-            bio: 'Elon Musk is a business magnate, industrial designer, and engineer. He is the founder, CEO, CTO, and chief designer of SpaceX; early investor, CEO, and product architect of Tesla, Inc.; founder of The Boring Company; co-founder of Neuralink; and co-founder and initial co-chairman of OpenAI.',
-            profession:'CEO of SpaceX',
+            fullName: 'Alan Turing',
+            bio: 'The Father of Modern Computing, Turing is recognized as a pioneer of computer science, cryptography, and artificial intelligence. He published a groundbreaking paper introducing the concept of the Turing machine. He also played a key role in breaking the German Enigma cipher',
+            profession:'Mathematician and logician',
             imgSrc: pic,
         }
-        this.state = {
+        this.state = { // A state to keep track of the time
             timer: 0
         }
-}
-
+    }
+// Component lifecycle method to start the timer
 componentDidMount() {
         this.timer = setInterval(() => {
-            this.setState({timer: this.state.timer + 1})}, 1000)
+            this.setState({timer: this.state.timer + 1})}, 1000) // Increment the timer every second
 }
 componentWillUnmount(){
-        clearInterval(this.timer)
+        clearInterval(this.timer) // Clear the timer when the component is unmounted
 }
 
   render() {
     return (
-      <div>
+      <div style={{margin: '50px'}}>
         <h5>{this.state.timer}</h5>
         <h2>{this.person.fullName}</h2>
-        <h3>{this.person.bio}</h3>
-        <h3>{this.person.profession}</h3>
-        <img src={this.person.imgSrc} alt="Elon Musk"/>
-
+        <h4>{this.person.bio}</h4>
+        <h4>{this.person.profession}</h4>
+        <img src={this.person.imgSrc} alt="Alan Turing" style={{width: '200px'}}/>
       </div>
     )
   }
